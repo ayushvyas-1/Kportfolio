cfe4343137ae1e730de6207704a61a449e5a92ae
Ayush Vyas <ayushkv360@gmail.com>
2025-11-17 15:12:19 +0530

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import 'react-photo-view/dist/react-photo-view.css';
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 createRoot(document.getElementById('root')!).render(
     <App />
